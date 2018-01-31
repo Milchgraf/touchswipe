2f92cd9765aed5b6b0d13b6bad59cc4592f09f19
Milchgraf <jpannicke@yahoo.de>
2018-02-01 00:23:10 +0100

    Slider with hammer js ready to use

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,42 +3,120 @@ function cl(text){
 }
 
 var container = document.getElementById('container');
-var innerBigContainer = document.getElementById('inner_big_container');
-var containerWidth = innerBigContainer.style.width;
+var containerJQ = $('#container');
+//var innerBigContainer = document.getElementById('inner_big_container');
+//var containerWidth = innerBigContainer.style.width;
 var displayWidth = screen.width;
-var startX;
-var offset = 0;
+var start,
+    end,
+    offset,
+    counter = 1,
+    oldLeft = 0,
+    newLeft = 0,
+    countPages = $('.inner_big_container').children().length,
+    oldRight;
 
-cl('Container: ' + container + '\nContainer width:\t' + containerWidth + '\nDisplay width:\t\t' + displayWidth);
+cl(countPages);
 
-function touchStart(evt){
-  startX = evt.touches[0].clientX;
-}
-function touchMove(evt){
-  var change = startX - evt.touches[0].clientX;
+var hammer = new Hammer(container);
 
-  if(change < 0){
-    return;
-  }
+//Recognize when PAN starts
+hammer.on('panstart', function(evt){
+  start = evt.center.x;
+});
 
-  innerBigContainer.style.left = '-' + (change + offset) + 'px';
-  cl('-' + change + offset + 'px');
+//During PAN gesture
+hammer.on('pan', function(evt){
 
-  evt.preventDefault();
-
-}
-function touchEnd(evt){
-  var change = (startX - evt.changedTouches[0].clientX);
-  var threshold = screen.width;
-
-  cl('CHANGE: ' + change + ' THRESHOLD: ' + threshold);
-
-  if(change < threshold){
-    innerBigContainer.style.transition = 'all .3s';
-    innerBigContainer.style.left = 0;
+  if(counter > 1){
+    offset = evt.deltaX - (containerJQ.width() * (counter - 1));
   }else{
-    innerBigContainer.style.transition = 'all .3s';
-    innerBigContainer.style.left = '-100%';
-    offset += screen.width;
+    offset = evt.deltaX;
   }
-}
+
+  $('.inner_big_container').css({
+    left: offset
+  });
+
+});
+
+//Recognize when PAN ends
+hammer.on('panend', function(evt){
+
+  end = evt.center.x;
+
+  //WENN DELTAX GRÖßER ALS SCREENWIDTH / 2
+  if(evt.deltaX < -containerJQ.width() / 2 || evt.deltaX > containerJQ.width() / 2){
+
+    //WENN NACH LINKS
+    if(start > end){
+      if(counter < countPages){
+        cl('PAN LEFT');
+        $('.inner_big_container').animate({
+          left: -100 * counter + '%'
+        }, 250);
+        counter += 1;
+        oldLeft -= 100;
+      }else{
+        cl('SNAPBACK LEFT PAN');
+        $('.inner_big_container').animate({
+          left: oldLeft + '%'
+        }, 250);
+      }
+
+    }
+    //WENN NACH RECHTS
+    if(start < end){
+      if(counter > 1 && oldLeft < 0){
+        cl('PAN RIGHT');
+        if(oldLeft != 0){
+          newLeft = oldLeft + 100;
+        }else{
+          newLeft = 0;
+        }
+
+        $('.inner_big_container').animate({
+          left: newLeft + '%'
+        }, 250);
+        counter -= 1;
+        oldLeft += 100;
+      }else{
+        cl('SNAPBACK RIGHT PAN');
+        $('.inner_big_container').animate({
+          left: 0
+        }, 250);
+      }
+    }
+
+  //WENN DELTAX KLEINER ALS SCREENWIDTH / 2
+  }else{
+
+    if(oldLeft != 0){
+      newLeft = oldLeft;
+    }else{
+      newLeft = 0;
+    }
+
+    //WENN NACH LINKS
+    if(start > end){
+      cl('SNAPBACK LEFT PAN');
+      $('.inner_big_container').animate({
+        left: newLeft + '%'
+      }, 250);
+    }
+    //WENN NACH RECHTS
+    if(start < end){
+      cl('SNAPBACK RIGHT PAN');
+      $('.inner_big_container').animate({
+        left: newLeft + '%'
+      }, 250);
+    }
+
+  }
+
+  cl('COUNTER: ' + counter);
+  cl('deltaX: ' + evt.deltaX);
+
+  $('.info_p').html('COUNTER: ' + counter + '<br>OLDLEFT: ' + oldLeft);
+
+});
